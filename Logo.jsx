f8de7914938e7b6c2273c1eed9e60de52b74e8ba
AppuMali <126770741+AppuMali@users.noreply.github.com>
2023-03-03 11:00:47 -0500

    Add files via upload

---
+++ Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo=()=>{
+    return(
+        <div>
+            {/* <img src="https://th.bing.com/th/id/R.b92bf19612e524ac1f9d5e998770eba7?rik=YHg0unj%2fGLXqRQ&riu=http%3a%2f%2fwww.car-brand-names.com%2fwp-content%2fuploads%2f2015%2f05%2fMercedes-symbol.jpg&ehk=VoRFHVAfoEX5td57bWcV%2b7%2fYJ3SzZLDH6VVMGjW9Ngs%3d&risl=&pid=ImgRaw&r=0"></img> */}
+       <img src="https://1000logos.net/wp-content/uploads/2018/04/Mercedes-Benz-Logo.png"></img>
+       </div>
+    )
+}
+export default Logo;
